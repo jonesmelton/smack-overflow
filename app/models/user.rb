72ea517ea4a52e2e920ff18f8f1da702eb0e79da
Jack Pults & Jones Melton <pair+jpults.shavah@devbootcamp.com>
2016-03-30 11:30:45 -0500

    Merge local changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,19 @@
 class User < ActiveRecord::Base
   # Remember to create a migration!
+  include BCrypt
 
   has_many :questions
   has_many :answers
   has_many :comments
   has_many :votes
+
+  def password
+    @password ||= Password.new(hashed_password)
+  end
+
+  def password=(new_password)
+    @password = Password.create(hashed_password)
+    self.hashed_password = @password
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 
 50.times do
 
-  User.create!(username: Faker::Superhero.name, email: Faker::Internet.free_email, password: "password")
+  User.create!(name: Faker::Superhero.name, email: Faker::Internet.free_email, password: "password")
 
 
 
