655ffcd099083f863b6c14d93bf05ec445bc4e7a
Emily Bosakowski & Byron Gage <pair+almightyboz.byronbenjamin@devbootcamp.com>
2016-03-30 11:32:02 -0500

    Change migration column names, add comment seed

---
+++ db/migrate/20160330102428_create_questions.rb
@@ -0,0 +1,12 @@
+class CreateQuestions < ActiveRecord::Migration
+  def change
+    create_table :questions do |t|
+      t.string :title
+      t.integer :best_answer_id
+      t.string :content, null: false
+      t.integer :user_id, null: false
+
+      t.timestamps(null: false)
+    end
+  end
+end
