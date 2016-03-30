a49360a83551e0bde7c32a9e2e8f28afd331a472
Emily Bosakowski & Byron Gage <pair+almightyboz.byronbenjamin@devbootcamp.com>
2016-03-30 16:03:32 -0500

    Write new question view and routes

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -3,6 +3,20 @@ get '/questions' do
   erb :'questions/index'
 end
 
+get '/questions/new' do
+  erb :'questions/new'
+end
+
+post '/questions' do
+  @question = Question.new(params[:question])
+  if @question.save
+    redirect '/questions'
+  else
+    erb :'questions/new'
+  end
+end
+
+
 
 get '/questions/:id' do
   @question = Question.find(params[:id])
