6dbd2086699bed86f09dd0b217b2c56e2df549bf
Emily Bosakowski & Jones Melton <pair+almightyboz.shavah@devbootcamp.com>
2016-03-31 11:29:20 -0500

    Add vote behavior

--- app/models/votable.rb
+++ app/models/votable.rb
@@ -2,4 +2,23 @@ module Votable
   def score
     self.votes.sum(:vote_value)
   end
+
+  def place_vote(vote_type, current_user)
+    # voted_object = self.class.find(self.id)
+    @vote = Vote.new(vote_value: tally_vote(vote_type),
+      user: current_user,
+      votable_type: self.class.to_s,
+      votable_id: self.id)
+    @vote.save
+  end
+
+  def tally_vote(vote_type)
+    if vote_type == "💩"
+      vote_value = -1
+    else
+      vote_value = 1
+    end
+
+  end
+
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,6 +2,6 @@ class Vote < ActiveRecord::Base
   belongs_to :user
   validates :user, presence: true
   belongs_to :votable, polymorphic: true
-  validates :user, uniqueness: { scope: :votable,
+  validates :user, uniqueness: { scope: :votable_id,
     message: "can only vote once." }
 end
