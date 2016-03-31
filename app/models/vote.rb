9ba708fa3da60a9fffdab931e2d5c739de48b981
Emily Bosakowski & Jones Melton <pair+almightyboz.shavah@devbootcamp.com>
2016-03-31 12:55:33 -0500

    Add vote uniqueness validation

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,6 +2,5 @@ class Vote < ActiveRecord::Base
   belongs_to :user
   validates :user, presence: true
   belongs_to :votable, polymorphic: true
-  validates :user, uniqueness: { scope: :votable_id,
-    message: "can only vote once." }
+  validates :user, uniqueness: { scope: [:votable_type, :votable_id] }
 end
